0983af357d129ae8072cab40ef76ea711fd3a55d
Nicole Bishop <nicolejayne915@hotmail.com>
2020-03-13 09:58:13 -0500

    addCategory and CategoryViewmodel

--- PizzaInventory1/Controllers/CategoryController.cs
+++ PizzaInventory1/Controllers/CategoryController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PizzaInventory1.Data;
+using PizzaInventory1.Models;
+using PizzaInventory1.ViewModels;
 
 namespace PizzaInventory1.Controllers
 {
@@ -17,7 +19,33 @@ namespace PizzaInventory1.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            List<ItemCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Add() 
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new category to my existing categories
+                ItemCategory newCategory = new ItemCategory
+                {
+                    Name = addCategoryViewModel.Name,
+                };
+
+                context.Categories.Add(newCategory);
+                //context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
         }
     }
 }
