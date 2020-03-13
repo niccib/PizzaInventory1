b521a0359ca9c406fda883cae86436cfd2969d82
Nicole Bishop <nicolejayne915@hotmail.com>
2020-03-13 01:08:29 -0500

    Added Category Controller and Model

---
+++ PizzaInventory1/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaInventory1.Data;
+
+namespace PizzaInventory1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ItemDbContext context;
+        
+        public CategoryController(ItemDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PizzaInventory1/Data/ItemDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaInventory1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaInventory1.Data
+{
+    public class ItemDbContext
+    {
+        public DbSet<ItemCategory> Categories { get; set; }
+    }
+}
